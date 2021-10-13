407bf7ae00395f2811b64902e8025b9b75012932
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-10-13 10:54:30 +0300

    Interface Props enum CardVariant  variant: CardVariant

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import Card from "./components/Card"
+import Card, { CardVariant } from "./components/Card"
 
 const App = () => {
   return (
     <div>
       <h1>СТАРТ</h1>
-      <Card width={'150px'} height={'150px'} >
+      <Card width={'150px'} height={'150px'} variant={CardVariant.primary}>
         <button>ЖМИ</button>
       </Card>
     </div>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,25 @@
 import React, { FC, ReactChild } from 'react'
 
+export enum CardVariant {
+  outlined = 'outlined',
+  primary = 'primary'
+}
+
 interface CardProps {
   width?: string
   height?: string
+  variant: CardVariant
 }
 
-const Card: FC<CardProps> = ({ width, height, children }) => {
+const Card: FC<CardProps> = ({ width, height, variant, children }) => {
   return (
-    <div style={{ width, height, border: '1px solid teal' }}>
+    <div style={{
+      width, height,
+      border: variant === CardVariant.outlined
+        ? '1px solid teal' : 'none',
+      background: variant === CardVariant.primary
+        ? 'teal' : 'none'
+    }}>
       {children}
     </div>
   )
